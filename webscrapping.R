35a9c923ffac36325021205c8b7356d1b9a04d3f
Abby Hunt <ahunt@smith.edu>
2025-04-27 17:29:25 -0400

    websrcrapping commit 4/27

--- webscrapping.R
+++ webscrapping.R
@@ -4,35 +4,50 @@
 #' @importFrom
 #' @export
 
+
+
+###LOADING PACKAGES
 #total number of rides on DollyWood website: 44
 library(rvest)
 library(stringr)
 library(tidyverse)
 
+
+
+###LOADING IN WEBPAGE
 dollywood <- rvest::read_html("https://www.dollywood.com/themepark/rides/")
 
-# Retrieves ride names
+
+###RIDE NAMES
 ride_names <- dollywood |>
   html_elements("h3") |>
   html_text()
 ride_names
 
-#Cleans the ride names, deletes duplicates, do we need to filter out stuff at the bottom (book your trip, help, etc?), after doing these steps we still have 44
+#Cleaing ride names
 ride_names <- unique(ride_names)
-ride_names <- ride_names[-c(45:50)] #gets rid of the last 5, which are functions on the websites and not attractions
+ride_names <- ride_names[-c(45:50)]
 ride_names
 
-# works to extract the links, we have 65 links, not super sure why? can filter it later
+
+
+###LINKS
 links <- dollywood |>
   html_elements(".resultItem a.hfe-cta-link") |>
   html_attr("href")
 
+links <- links[grepl("/themepark/rides/", links)]
+links <- unique(links)
 links <- paste0("https://www.dollywood.com", links)
+length(links)
+
 
 rvest::read_html(links[1])
 rvest::read_html(links[2])
 
-#location of rides, 85 results (includes timesaver which we don't need)
+
+###RIDE LOCATIONS
+
 ride_location <- dollywood |>
   html_elements(".result-attributes div") |>
   html_text()
@@ -49,8 +64,8 @@ ride_locations_clean <- str_squish(ride_location)
 valid_indices <- ride_locations_clean != "TimeSaver Pass"
 
 # filtered ride locations, no more timesaver
-ride_names_filtered <- ride_names_clean[valid_indices] #this ends up getting rid of some of the rides, adding a bunch of NA values, not sure why so maybe we don't use it?
-ride_locations_filtered <- ride_locations_clean[valid_indices] #this doesn't get rid of timesaver premium only, can do that further down
+ride_names_filtered <- ride_names_clean[valid_indices]
+ride_locations_filtered <- ride_locations_clean[valid_indices]
 
 valid_indices2 <- ride_locations_filtered != "TimeSaver Premium Only"
 ride_locations_filtered <- ride_locations_filtered[valid_indices2]
@@ -77,17 +92,29 @@ ride_locations_filtered <- ride_locations_filtered[-c(5:8, 10, 17:18, 23, 25:26,
 ride_locations_filtered <- c("Wildwood Grove", as.list(ride_locations_filtered))
 ride_locations_filtered <- append(ride_locations_filtered[-1], ride_locations_filtered[1], 42)
 
-links_filtered <- links[valid_indices] #doing this adds two more links and makes a bunch of them NA? i dont think that there are any time saver functions in the links but there are duplicates, so maybe we do the unique() function instead??
 
-links_filtered <- unique(links) #something like this?? this also gets the NA count down
 
+
+###RIDE PICTURES
 #ride pictures, can't get this to work LOL
 ride_pictures <- dollywood |>
   html_elements(".result-image img") |>
   html_text()
 ride_pictures
 
-ride_descriptions <- sapply(links_filtered, function(link) {
+images <- dollywood |>
+  html_elements(".resultItem img") |>
+  html_attr("src")
+
+images <- unique(images)
+images <- images [1:44]
+head(images)
+
+
+
+
+###RIDE DESCRIPTIONS
+ride_descriptions <- sapply(links, function(link) {
   tryCatch({
     page <- read_html(link)
     desc <- page |>
@@ -105,7 +132,7 @@ ride_descriptions <- sapply(links_filtered, function(link) {
 sum(is.na(ride_descriptions))  # Should show how many links failed - 0!
 
 rides_df <- tibble(
-  name = ride_names_clean,
+  name = ride_names,
   location = ride_locations_filtered,
   description = ride_descriptions
 )
@@ -113,8 +140,5 @@ rides_df
 
 #want to filter out the "" blanks
 remove_blanks <- rides_df[!str_detect(rides_df$description,  ""), ]
-
-
-
 
 
